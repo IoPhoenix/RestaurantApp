20e4a3c15dfa1d75c6bb46100d871b37c5870837
IoPhoenix <olga.fomin26@gmail.com>
2018-08-07 17:05:26 -0700

    Finished gulp task to minify and concat js files

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,17 +1,36 @@
 const gulp = require('gulp'),
-    pump = require('pump'),
-    uglify = require('gulp-uglify');
+    del = require('del'),
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify');
+
+    // remove previous version of minified js
+    gulp.task('deleteMinifiedScripts', function() {
+        return del(['./app/assets/scripts/app.min.js', './app/assets/scripts/restaurant.min.js']);
+    });
+
+    gulp.task('minifyRestaurantScript', ['deleteMinifiedScripts'], function() {
+        return gulp.src(['./app/assets/scripts/dbhelper.js', './app/assets/scripts/restaurant_info.js', './app/assets/scripts/toggleMenu.js'])
+        .pipe(concat('restaurant.js'))
+          .pipe(minify({
+            ext:{
+                min:'.min.js'
+            },
+            noSource: true
+          }))
+          .pipe(gulp.dest('./app/assets/scripts/'))
+      });
 
 
-gulp.task('scripts', function (cb) {
-    pump([
-        gulp.src('./app/assets/scripts/*.js'),
-        uglify(),
-        gulp.dest('./app/assets/scripts/*.js')
-    ],
-    cb
-    );
-});
+    gulp.task('scripts', ['minifyRestaurantScript'], function() {
+        return gulp.src(['./app/assets/scripts/dbhelper.js', './app/assets/scripts/main.js'])
+        .pipe(concat('app.js'))
+          .pipe(minify({
+            ext:{
+                min:'.min.js'
+            },
+            noSource: true
+          }))
+          .pipe(gulp.dest('./app/assets/scripts/'))
+      });
 
-
-
+   
