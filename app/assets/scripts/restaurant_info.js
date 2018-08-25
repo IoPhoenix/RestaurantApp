e0dfcb150e5bb4fdf714f75e2b695a319d424696
IoPhoenix <olga.fomin26@gmail.com>
2018-08-24 18:22:59 -0700

    Added review form

--- app/assets/scripts/restaurant_info.js
+++ app/assets/scripts/restaurant_info.js
@@ -2,22 +2,6 @@ let restaurant;
 var newMap;
 
 
-// window.initMap = () => {
-//   fetchRestaurantFromURL((error, restaurant) => {
-//     if (error) { // Got an error!
-//       console.error(error);
-//     } else {
-//       self.map = new google.maps.Map(document.getElementById('map'), {
-//         zoom: 16,
-//         center: restaurant.latlng,
-//         scrollwheel: false
-//       });
-//       fillBreadcrumb();
-//       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
-//     }
-//   });
-// }
-
 document.addEventListener('DOMContentLoaded', (event) => {  
   initMap();
 });
